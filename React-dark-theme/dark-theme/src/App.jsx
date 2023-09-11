2ed08c289a6db7a198aee57ae308a05cafaa4569
seyitkarahan <karahanseyit570@gmail.com>
2023-09-11 10:43:12 +0300

    React dark theme
    I made react dark theme

---
+++ React-dark-theme/dark-theme/src/App.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+import useLocalStorage from "use-local-storage"
+
+import "./index.css"
+
+function App() {
+
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light")
+
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme)
+  }
+
+  return (
+    <div className="app" data-theme={theme}>
+      <div className="login">
+        <h1>Login</h1>
+        <div className="container">
+            <div className="top">
+              <i className="fab fa-google"></i>
+              <i className="fab fa-facebook-square"></i>
+              <i className="fab fa-linkedin"></i>
+              <i className="fab fa-twitter-square"></i>
+              <i className="fab fa-apple"></i>
+            </div>
+            <p className="divider"><span>or</span></p>
+            <form>
+              <label>E-mail</label>
+              <input type="email" placeholder="Enter your email" />
+              <label>Password</label>
+              <input type="password" placeholder="Enter your password" />
+              <div className="remember">
+                <input type="checkbox" checked="checked" />
+                <p>Remember Me</p>
+              </div>
+              <button>Log In</button>
+            </form>
+            <div className="bottom">
+              <p>Forget your password?</p>
+              <a href="#">Reset Password</a>
+            </div>
+            <p className="create">Create Account</p>
+        </div>
+        <div className="theme-toggle">
+          <h2>Light Theme</h2>
+          <i onClick={switchTheme} className="fas fa-toggle-on"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
